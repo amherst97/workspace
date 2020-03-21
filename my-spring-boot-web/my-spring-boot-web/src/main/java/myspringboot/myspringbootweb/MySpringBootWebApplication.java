5285b7263c53179d55d75c2ce73361afdafd6c9a
Dansong Chen <dansong.chen@gmail.com>
2020-03-21 13:01:44 -0400

    Add log for main class

--- my-spring-boot-web/my-spring-boot-web/src/main/java/myspringboot/myspringbootweb/MySpringBootWebApplication.java
+++ my-spring-boot-web/my-spring-boot-web/src/main/java/myspringboot/myspringbootweb/MySpringBootWebApplication.java
@@ -1,13 +1,17 @@
 package myspringboot.myspringbootweb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MySpringBootWebApplication {
+	private static Logger logger = LoggerFactory.getLogger(MySpringBootWebApplication.class);
 
 	public static void main(String[] args) {
 
+		logger.info("Starting");
 		SpringApplication.run(MySpringBootWebApplication.class, args);
 	}
 
