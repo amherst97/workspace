04a30caa95921e2f109f4402c4ffdd2e5d41ce22
Dansong Chen <dansong.chen@gmail.com>
2020-12-28 18:30:22 -0500

    Add registration controller

--- conference-boot/src/main/java/com/home/conferenceboot/controller/RegistrationController.java
+++ conference-boot/src/main/java/com/home/conferenceboot/controller/RegistrationController.java
@@ -1,2 +1,15 @@
-package com.home.conferenceboot.controller;public class RegistrationController {
+package com.home.conferenceboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("registration")
+    public String getRegistration(Map<String, Object> model) {
+        return "registration";
+    }
 }
